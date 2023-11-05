2f50fed0b6ddd9b365bed9436b4dc9d150b3fcf8
MBoota <m43577535@gmail.com>
2023-11-05 12:05:14 +0500

    adding  RepositoryOf Product with IUnit

--- Proj.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ Proj.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -9,6 +9,7 @@ namespace Proj.DataAccess.Repository.IRepository
     public interface IUnitOfWork
     {
         public ICategoryRepository Category { get;}
+        public IProductRepository Product { get; }
         void SaveChange();
     }
 }
---
+++ Proj.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using Proj.DataAccess.Data;
+using Proj.DataAccess.Repository.IRepository;
+using Proj.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
